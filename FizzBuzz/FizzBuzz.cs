7305e66693b9050d0419b84740c26569575f356d
Maciek-laptop <Maciek-laptop@MACIEK_LAPTOP>
2017-07-26 18:06:23 +0200

    Dodaj pliki projektów.

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+        private const int Fizzing = 3;
+        private const int Buzzing = 5;
+
+        public bool IsFizz(int number)
+        {
+            return number % Fizzing == 0;
+        }
+        public bool IsBuzz(int number)
+        {
+            return number % Buzzing == 0;
+        }
+        public bool IsFizzBuzz(int number)
+        {            
+            //return IsFizz(number) && IsBuzz(number); - can't decide which option is more efficient 
+            return number % (Fizzing * Buzzing) == 0;
+        }
+
+        public string Go(int number)
+        {
+            if (IsFizzBuzz(number)) return "FizzBuzz";
+            if (IsBuzz(number)) return "Buzz";
+            if (IsFizz(number)) return "Fizz";
+            return number.ToString();
+        }
+
+        public string[] GetSequence(int range)
+        {
+            var sequence = new List<string>();
+            for (int i = 1; i <= range; i++)
+            {
+                sequence.Add(Go(i));
+            }
+            return sequence.ToArray();
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzzTest.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzTest
+    {
+        private FizzBuzz _fizzBuzz;
+
+        public FizzBuzzTest()
+        {
+            _fizzBuzz = new FizzBuzz();
+        }
+
+        public void Is_Zero_Fizz_Must_Return_False()
+        {
+            var result = _fizzBuzz.IsFizz(0);
+            Assert.Equal(result, false);
+        }
+        [Theory]
+        [InlineData(3)]
+        [InlineData(6)]
+        [InlineData(9)]
+        [InlineData(12)]
+        [InlineData(18)]
+        [InlineData(21)]
+        public void Is_Fizz_Must_Return_True(int value)
+        {
+            var result = _fizzBuzz.IsFizz(value);
+            Assert.Equal(result, true);
+        }
+        [Fact]
+        public void Is_Five_Fizz_Must_Return_False()
+        {
+            var result = _fizzBuzz.IsFizz(5);
+            Assert.Equal(result, false);
+        }
+        [Fact]
+        public void Is_Ten_Buzz_Must_Return_True()
+        {
+            var result = _fizzBuzz.IsBuzz(10);
+            Assert.Equal(result, true);
+        }
+        [Theory]
+        [InlineData(15)]
+        [InlineData(30)]
+        [InlineData(45)]
+        [InlineData(60)]
+        [InlineData(75)]
+        [InlineData(90)]
+        public void Is_Value_FizzBuzz_Must_Return_True(int value)
+        {
+            var result = _fizzBuzz.IsFizzBuzz(value);
+            Assert.Equal(result, true);
+        }
+
+        [Fact]
+        public void Zero_Must_Return_FizzBuzz()
+        {
+            var result = _fizzBuzz.Go(0);
+            Assert.Equal(result, "FizzBuzz");
+        }
+
+        [Fact]
+        public void Three_Must_Return_Fizz()
+        {
+            var result = _fizzBuzz.Go(3);
+            Assert.Equal(result, "Fizz");
+        }
+        
+        [Fact]
+        public void Five_Must_Return_Buzz()
+        {
+            var result = _fizzBuzz.Go(5);
+            Assert.Equal(result, "Buzz");
+        }
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(4)]
+        [InlineData(13)]
+        [InlineData(86)]
+        [InlineData(1674937)]
+        public void Neutral_Must_Return_Same_Value(int value)
+        {
+            var result = _fizzBuzz.Go(value);
+            Assert.Equal(result, value.ToString());
+        }
+        
+        [Fact]
+        public void Check_Sequence()
+        {
+            var result = _fizzBuzz.GetSequence(5);
+            string[] expectedResult = { "1", "2", "Fizz", "4", "Buzz" };
+            Assert.Equal(result, expectedResult);
+        }
+
+    }
+}
